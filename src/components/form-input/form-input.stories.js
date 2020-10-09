fdc00011ce6d3a1a0c8d321b4d73e84af9d31ed1
ncbecker <nbecker.ac@gmail.com>
2020-10-09 12:32:39 +0200

    Add basic input structure

---
+++ src/components/form-input/form-input.stories.js
@@ -0,0 +1,9 @@
+import "./form-input.css";
+import forminput from "./form-input.html";
+
+export default {
+  title: "Components/Input",
+  parameters: { layout: "centered" },
+};
+
+export const formInput = () => forminput;
